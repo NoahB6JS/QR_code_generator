bebadaa16c5525a2c1222829aadc100dc5e6712f
NoahB6JS <noah.js.b6@gmail.com>
2025-12-15 19:03:28 +0000

    main GUI code
    label and entry

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import tkinter
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x250")
+root.title("QR code generator")
+
+
+text_title = tk.StringVar()
+text_title.set("QR CODE GENERATOR")
+
+label = tk.Label(root,
+                 textvariable=text_title,
+                 anchor=tk.CENTER,
+                 )
+
+entry_label = tk.Label(root, textvariable="URL:  ")
+entry = tk.Entry(root)
+
+label.pack(pady=20)
+entry_label.pack(pady=0)
+entry.pack(pady=20)
+
+root.mainloop()
