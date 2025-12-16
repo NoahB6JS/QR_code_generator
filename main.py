ba7b25659d5f50eaa413b6d4d65943a11a9d26ca
NoahB6JS <noah.js.b6@gmail.com>
2025-12-16 12:11:22 +0000

    Button collects urls
    Button added:
    
    - collects data in entry widget
    - prints to terminal
    
    Need to do:
    
    - Clear entry when the button is clicked.
    - Do the backend development creating QR code. - import QR module

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ root = tk.Tk()
 root.geometry("400x250")
 root.title("QR code generator")
 
-
 text_title = tk.StringVar()
 text_title.set("QR CODE GENERATOR")
+
+#collecting widget data
+def get_url():
+    url = entry.get()
+    print(url)
 
 label = tk.Label(root,
                  textvariable=text_title,
                  anchor=tk.CENTER,
                  )
 
-entry_label = tk.Label(root, textvariable="URL:  ")
-entry = tk.Entry(root)
+entry_label = tk.Label(root, text="URL:  ")
 
+entry = tk.Entry(root)
+button = tk.Button(root, text="Generate", command=get_url)
+
+
+#packing all widgets
 label.pack(pady=20)
 entry_label.pack(pady=0)
 entry.pack(pady=20)
+button.pack()
 
 root.mainloop()
